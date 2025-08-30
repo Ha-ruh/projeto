032ace9c9a0db37fcd69edf7f3ff4c755e7d92ce
Samuel de Oliveira <samueldeoliveih@gmail.com>
2025-08-29 21:35:23 -0300

    :tada: initial commit

---
+++ Script.js
@@ -0,0 +1,13 @@
+function ToggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+    
+    
+    const img = document.querySelector("#profile img")
+
+    if (html.classList.contains("light")) {
+        img.setAttribute ("src", "./assets/Rimuru.png")
+    } else {
+        img.setAttribute ("src", "./assets/Yoshi.png")
+    }
+}
